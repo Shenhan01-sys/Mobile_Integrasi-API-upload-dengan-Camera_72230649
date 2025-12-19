7b03652c4774a85191d2a263cfcdfcde30891723
hans gunawan <hansgunawan@gmail.com>
2025-12-19 18:07:32 +0700

    fixing issues

---
+++ StudiBase/StudiBase.Web/Migrations/20251219105746_UpdateSchema.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudiBase.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Trainers",
+                type: "REAL",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Trainers",
+                type: "REAL",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Trainers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { null, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Trainers");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Trainers");
+        }
+    }
+}
